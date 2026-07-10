9f282128736e08eabddd1b98099420c850144bc5
rafireyhan <redan215@gmail.com>
2026-07-10 11:14:10 +0700

    refactor(web): remove auth, sidebar, admin pages; add anon supabase client

--- panel/lib/tags.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from 'vitest'
-import { isValidStatus, nextStatuses } from './tags'
-
-test('valid statuses', () => {
-  expect(nextStatuses()).toEqual(['new', 'in_progress', 'resolved'])
-  expect(isValidStatus('resolved')).toBe(true)
-  expect(isValidStatus('done')).toBe(false)
-})
